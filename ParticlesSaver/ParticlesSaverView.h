3cca313d7eb2a7c221129e40c2477dc1e49fcf4a
Jonic Linley <jonic@100yen.co.uk>
2013-01-26 20:56:24 +0000

    Initial Commit

---
+++ ParticlesSaver/ParticlesSaverView.h
@@ -0,0 +1,13 @@
+//
+//  ParticlesSaverView.h
+//  ParticlesSaver
+//
+//  Created by Jonic Linley on 26/01/2013.
+//  Copyright (c) 2013 HelicopterExplosion. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ParticlesSaverView : ScreenSaverView
+
+@end
